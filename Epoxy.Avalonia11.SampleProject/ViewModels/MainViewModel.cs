11040f47d51b2e6fd575e9c4c588bc8c3b62ab78
Kouji Matsui <k@kekyo.net>
2024-05-15 11:28:58 +0900

    Initialize property from strictly defined constructor.

--- Epoxy.Avalonia11.SampleProject/ViewModels/MainViewModel.cs
+++ Epoxy.Avalonia11.SampleProject/ViewModels/MainViewModel.cs
@@ -3,7 +3,12 @@
 [ViewModel]
 public class MainViewModel
 {
+    public MainViewModel()
+    {
+        this.Greeting = "Welcome to Avalonia!";
+    }
+
 #pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
+    public string Greeting { get; }
 #pragma warning restore CA1822 // Mark members as static
 }
