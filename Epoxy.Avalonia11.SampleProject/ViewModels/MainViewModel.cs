9a863cd745a0434bdbbd5d7ad52236f4d1ed538b
Kouji Matsui <k@kekyo.net>
2024-05-15 11:46:12 +0900

    Set property into a loaded event by `Fountain/Well`.

--- Epoxy.Avalonia11.SampleProject/ViewModels/MainViewModel.cs
+++ Epoxy.Avalonia11.SampleProject/ViewModels/MainViewModel.cs
@@ -1,14 +1,22 @@
-﻿namespace Epoxy.Avalonia11.SampleProject.ViewModels;
+﻿using Avalonia.Controls;
+
+namespace Epoxy.Avalonia11.SampleProject.ViewModels;
 
 [ViewModel]
 public class MainViewModel
 {
     public MainViewModel()
     {
-        this.Greeting = "Welcome to Avalonia!";
+        this.MainViewWell.Add(Control.LoadedEvent, () =>
+        {
+            this.Greeting = "Welcome to Avalonia!";
+            return default;
+        });
     }
 
 #pragma warning disable CA1822 // Mark members as static
-    public string Greeting { get; }
+    public string? Greeting { get; private set; }
+
+    public Well<UserControl> MainViewWell { get; } = Well.Factory.Create<UserControl>();
 #pragma warning restore CA1822 // Mark members as static
 }
